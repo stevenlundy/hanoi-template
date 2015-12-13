f8620185e721f6cb3c7b937e0a77007b23155558
stevenlundy <steven.lundy@gmail.com>
2015-12-13 20:42:03 +0000

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-
+function drawTower(towerNum, blocks){
+  var tower = document.getElementById("tower"+towerNum);
+  tower.innerHTML = "";
+  for(var i = 0; i < blocks.length; i++){
+    var block = document.createElement("div");
+    block.classList.add("block");
+    block.setAttribute("id", "block"+blocks[i]);
+    tower.appendChild(block);
+  }
+}
+function updateCounter(num){
+    var counter = document.getElementById("counter");
+    counter.innerHTML = num;
+}
+updateCounter(0);
+drawTower(1,[1,2,4]);
+drawTower(2,[3,5]);
+drawTower(3,[]);
